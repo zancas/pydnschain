b5878f4ad3732bd47f53a1a6dde998516f2dba0e
zancas <zancas@leastauthority.com>
2014-10-19 17:27:12 -0600

    Basic synchronous timing out, urllib request.

--- dnschain/server.py
+++ dnschain/server.py
@@ -1,4 +1,4 @@
-import json
+import json, urllib, httplib
 
 
 class Server:
@@ -17,6 +17,7 @@ class Server:
 
         self.addr = addr
         self.fingerprint = fingerprint
+        self.connection = httplib.HTTPConnection(addr)
 
     def lookup(self, name):
         """
@@ -26,7 +27,21 @@ class Server:
 
         @param name: The name to lookup, e.g. 'id/dionyziz'
         """
-
-        # namecoin_string = read_from_dnschain(name)
-        data = json.loads(namecoin_string)
+        #uname = name.encode("utf-8")#Who knows what bytes will be submitted?! Look up dnschain spec.
+        url_safe_name = urllib.quote(name, safe="") #Meant to be a url path _component_.
+        query_string = "http://%s/id/%s" % (self.addr, url_safe_name)
+        namecoin_response = urllib.urlopen(query_string)
+        namecoin_string = namecoin_response.read()
+        try:
+            data = json.loads(namecoin_string)
+        except ValueError, e:
+            print "Instead of JSON we got this:\n%s\n" % (e,)
+            import sys
+            sys.exit(99) #Fail hard.
         return data
+
+
+if __name__ == '__main__':
+    #DNSChainServer = Server("192.184.93.146", "NOTYETIMPLEMENTED")#Seems to coerce to https. 443?
+    DNSChainServer = Server("dns.dnschain.net", "NOTYETIMPLEMENTED")
+    print DNSChainServer.lookup("greg")
